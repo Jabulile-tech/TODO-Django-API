6612c527cb464985ce8fa089de4e5436dc0953e3
Jabulile-tech <jabulilembaligamede@gmail.com>
2026-06-02 14:13:27 +0200

    Add root redirect to /api/

--- OneDrive/Documentos/Tati Software Technical Assessment/todo_api/urls.py
+++ OneDrive/Documentos/Tati Software Technical Assessment/todo_api/urls.py
@@ -3,8 +3,10 @@ URL configuration for todo_api project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
 ]
